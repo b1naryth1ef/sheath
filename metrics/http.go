670ba57f86101fe6fa45ea359dd421e0a47aa5f7
andrei <b1naryth1ef@gmail.com>
2024-11-13 00:46:15 -0800

    cleanup(http): add request latency / cleanup stuff

--- metrics/http.go
+++ metrics/http.go
@@ -2,24 +2,38 @@ package metrics
 
 import (
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
-func metricsMiddleware(next http.Handler) http.Handler {
+// MetricsMiddleware is an HTTP middleware that tracks chi/v5 requests with Prometheus
+func MetricsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		next.ServeHTTP(w, r)
+		start := time.Now()
+		ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
+
+		next.ServeHTTP(ww, r)
 
 		routePattern := chi.RouteContext(r.Context()).RoutePattern()
-		httpRequests.WithLabelValues(r.Method, routePattern).Inc()
+		httpRequests.WithLabelValues(r.Method, routePattern, strconv.Itoa(ww.Status())).Inc()
+		httpRequestLatency.WithLabelValues(r.Method, routePattern, strconv.Itoa(ww.Status())).Observe(time.Since(start).Seconds())
 	})
 }
 
 var (
 	httpRequests = promauto.NewCounterVec(prometheus.CounterOpts{
-		Name: "go_http_requests",
+		Name: "go_http_request_count",
 		Help: "The total number of http requests",
-	}, []string{"method", "route"})
+	}, []string{"method", "route", "status"})
+
+	httpRequestLatency = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Name:    "go_http_request_duration_seconds",
+		Help:    "Histogram of request latency in seconds",
+		Buckets: []float64{.005, .01, .025, .05, .1, .25, .5, 1, 2.5, 5, 10},
+	}, []string{"method", "route", "status"})
 )
